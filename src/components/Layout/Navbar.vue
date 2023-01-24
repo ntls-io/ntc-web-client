c0c16c554a585fd8e1581828591e79cda9fac341
Jonathan Ngbonga <jonathan@ntls.io>
2023-01-24 13:38:20 +0200

    feat: add nav profile

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,11 +1,31 @@
 <template>
-  <b-navbar class="main-header bg-white border-0">
-    <b-nav class="navbar-nav" pills>
-      <b-nav-item @click="toggle()">
-        <b-icon icon="layout-sidebar-inset"></b-icon>
-      </b-nav-item>
-    </b-nav>
-  </b-navbar>
+  <nav class="main-header navbar navbar-expand navbar-light border-0">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          data-widget="pushmenu"
+          href="#"
+          role="button"
+          @click="toggle()"
+        >
+          <b-icon icon="layout-sidebar"></b-icon
+        ></a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+      <b-nav-item-dropdown text="Full Name" toggle-class="text-dark ">
+        <b-dropdown-item>
+          Profile
+        </b-dropdown-item>
+        <b-dropdown-item href="#">
+          Logout
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+    </ul>
+  </nav>
 </template>
 
 <script>
