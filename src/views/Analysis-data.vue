c96f3a92b1f532af12cd531c8fb948aef8715aae
Jonathan Ngbonga <jonathan@ntls.io>
2023-02-27 15:40:38 +0100

    refactor: update analysis & package tables

--- src/views/Analysis-data.vue
+++ src/views/Analysis-data.vue
@@ -2,22 +2,31 @@
   <b-container fluid>
     <b-row>
       <b-col>
-        <b-table
-          head-variant="dark"
-          fixed
-          show-empty
-          :fields="fields"
-          :items="items"
-        >
-          <template v-slot:cell(actions)>
-            <div class="d-flex justify-content-around">
-              <b-button size="sm" variant="outline-dark">Sell </b-button>
-              <b-button size="sm" variant="outline-dark"
-                >View results
-              </b-button>
-            </div>
-          </template>
-        </b-table>
+        <b-card no-body>
+          <b-row class="my-3 mx-1">
+            <b-col cols="auto">
+              <b-form-group class="m-0">
+                <b-form-input type="text" placeholder="Search"></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-table
+            head-variant="dark"
+            fixed
+            show-empty
+            :fields="fields"
+            :items="items"
+          >
+            <template v-slot:cell(actions)>
+              <div class="d-flex justify-content-around">
+                <b-button size="sm" variant="outline-dark">Sell </b-button>
+                <b-button size="sm" variant="outline-dark"
+                  >View results
+                </b-button>
+              </div>
+            </template>
+          </b-table>
+        </b-card>
       </b-col>
     </b-row>
   </b-container>
