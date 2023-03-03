28535b7f8a64e4fc3e9bd41bec5c69ee886ac013
Jonathan Ngbonga <jonathan@ntls.io>
2023-03-03 20:35:23 +0100

    style(swal): add custom class for confirm button

--- src/plugins/sweatalert.js
+++ src/plugins/sweatalert.js
@@ -4,4 +4,9 @@ import VueSweetalert2 from "vue-sweetalert2";
 
 import "sweetalert2/dist/sweetalert2.min.css";
 
-Vue.use(VueSweetalert2);
+const options = {
+  customClass: {
+    confirmButton: "bg-dark"
+  }
+};
+Vue.use(VueSweetalert2, options);
